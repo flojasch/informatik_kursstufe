5a05a2cf792392e58af463abfc25a5ca6e35bc8e
flojasch <f.jasch@gmx.net>
2019-10-27 09:48:43 +0100

    3Dbaum

--- rekursive_grafik/3dBaum.js
+++ rekursive_grafik/3dBaum.js
@@ -1,11 +1,14 @@
 let gen;
 let slider;
+let pslider;
 let angle;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   slider = createSlider(0, PI / 2, PI/3, 0.01);
   slider.position(50,50);
+  pslider=createSlider(-PI/2,PI/2,0,0.01);
+  pslider.position(50,70);
   gen = 0;
 }
 
@@ -17,36 +20,41 @@ function draw() {
   background(100);
   directionalLight(250, 250, 250, -1, -1, -0.5);
   ambientLight(100, 100, 100);
-  fill(130, 69, 4);
   noStroke();
   angle = slider.value();
-  rotateY(frameCount*0.01);
+  pangle=pslider.value();
   translate(0,height/2,0);
-  baum(height / 2, 0);
+  rotateY(frameCount*0.01);
+  rotateX(pangle);
+  baum(height / 3,20,1,0);
 }
 
-function baum(h, g) {
+function baum(h,b,f,g) {
+  fill(114+f*(130-114),181 +f*(69-181),38 +f*(4-38));
   translate(0,-h/2,0);
-  cylinder(20,h);
+  cylinder(b,h);
   if (g >= gen) {
     return;
   }
   translate(0,-h/2,0);
-  
+  h *=0.67;
+  b *=0.9;
+  f *=0.9;
+  g+=1;
   push();
   rotateZ(angle);
-  baum(h*0.67,g+1);
+  baum(h,b,f,g);
   pop();
 
   push();
-  rotateZ(-angle);
-  baum(h*0.67,g+1);
+  rotateY(TWO_PI/3)
+  rotateZ(angle);
+  baum(h,b,f,g);
   pop();
 
   push();
-  rotateY(PI/2);
+  rotateY(PI/3);
   rotateZ(-angle);
-  baum(h*0.67,g+1);
+  baum(h,b,f,g);
   pop();
-
 }
