60892260d70780ec8734f6a719b858e37c6efed1
flojasch <f.jasch72@gmail.com>
2019-11-05 18:45:03 +0100

    sierpinsky

--- rekursive_grafik/sierpinsky.js
+++ rekursive_grafik/sierpinsky.js
@@ -1,37 +1,43 @@
-let faktor;
-let slider;
 let alphaslider;
 let alpha;
+let maxgen;
 
 function setup() {
-  createCanvas(windowHeight, windowHeight);
-  slider=createSlider(5,40,10,1);
-  alphaslider=createSlider(0,PI/2,PI/4,0.01);
+  createCanvas(windowWidth, windowHeight);
+  slider=createSlider(0,PI/2,0,0.01);
+  slider.position(50,50);
+  maxgen=0;
+}
+
+function mousePressed(){
+  ++maxgen;
 }
 
 function draw(){ 
-  faktor=slider.value();
-  alpha=alphaslider.value();
-  background(100);
+  alpha=slider.value();
+  background(250);
   translate(0,height);
-  sierpinsky(width,0,0,0);
+  sierpinsky(height,0,0,0,0);
 }
 
-function sierpinsky(breite,r,g,b){
-  if(breite<1){
-    stroke(r,g,b);
-    point(0,0);
+function sierpinsky(breite,gen,r,g,b){
+  if(gen>=maxgen){
+    //stroke(r,g,b);
+    //point(0,0);
+    noStroke();
+    fill(r,g,b);
+    triangle(0,0,breite/2,-breite,breite,0);
     return;
   } 
-  sierpinsky(breite*0.5,r+faktor,g,b);
+  gen++;
+  sierpinsky(breite*0.5,gen,r+255/pow(2,gen),g,b);
   push();
   translate(breite*0.5,0);
-  sierpinsky(breite*0.5,r,g+faktor,b);
+  sierpinsky(breite*0.5,gen,r,g+255/pow(2,gen),b);
   pop();
   push();
   translate(breite/4,-breite/2);
   rotate(alpha);
-  sierpinsky(breite*0.5,r,g,b+faktor);
+  sierpinsky(breite*0.5,gen,r,g,b+255/pow(2,gen));
   pop();
-
 }
